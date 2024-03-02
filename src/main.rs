617c8f7e2ec917f3703a955bf6c7bd97c1c90e61
Thomas Solecki <thomasjsolecki@gmail.com>
2024-03-01 17:23:38 -0800

    Modify WeatherDisplay to show current, daily, and hourly weather

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,8 @@
-use askama::Template;
-use axum::extract::{Query, State};
+use core::panic;
+
+use chrono::{DateTime, Datelike, TimeZone, Timelike, Utc};
+// use askama::Template;
+use axum::extract::{Json, Query, State};
 use axum::http::StatusCode;
 use axum::routing::get;
 use axum::Router;
@@ -42,52 +45,199 @@ struct WeatherParams {
 struct WeatherResponse {
     latitude: f64,
     longitude: f64,
+    utc_offset_seconds: i64,
     timezone: String,
+    timezone_abbreviation: String,
+    current: Current,
     hourly: Hourly,
+    daily: Daily,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+struct Current {
+    time: i64,
+    temperature_2m: f64,
+    weather_code: u16,
 }
 
 #[derive(Serialize, Deserialize, Debug)]
 struct Hourly {
-    time: Vec<String>,
+    time: Vec<i64>,
     temperature_2m: Vec<f64>,
     weather_code: Vec<u16>,
 }
 
-#[derive(Template, Serialize, Deserialize, Debug)]
-#[template(path = "weather.html")]
+#[derive(Serialize, Deserialize, Debug)]
+struct Daily {
+    time: Vec<i64>,
+    temperature_2m_max: Vec<f64>,
+    temperature_2m_min: Vec<f64>,
+    weather_code: Vec<u16>,
+}
+
+// #[derive(Template, Serialize, Deserialize, Debug)]
+// #[template(path = "weather.html")]
+// struct WeatherDisplay {
+//     display_name: String,
+//     forecasts: Vec<Forecast>,
+// }
+
+#[derive(Serialize, Deserialize, Debug)]
 struct WeatherDisplay {
     display_name: String,
-    forecasts: Vec<Forecast>,
+    current: CurrentForecast,
+    hourly: Vec<HourlyForecast>,
+    daily: Vec<DailyForecast>,
 }
 
 #[derive(Serialize, Deserialize, Debug)]
-struct Forecast {
+struct CurrentForecast {
+    time: String,
+    temp_max: f64,
+    temp_min: f64,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+struct HourlyForecast {
     date: String,
     temperature: f64,
     weather_code: String,
 }
 
-fn celsius_to_fahrenheit(celsius: &f64) -> f64 {
-    // Convert Celsius to Fahrenheit using the formula: (C × 9/5) + 32
-    (*celsius * 9.0 / 5.0) + 32.0
+#[derive(Serialize, Deserialize, Debug)]
+struct DailyForecast {
+    date: String,
+    temperature_min: f64,
+    temperature_max: f64,
+    weather_code: String,
 }
+
+struct DailyForecastWithDateTime {
+    date: DateTime<Utc>,
+    temperature_min: f64,
+    temperature_max: f64,
+    weather_code: u16,
+}
+
+struct HourlyForecastWithDateTime {
+    date: DateTime<Utc>,
+    temperature: f64,
+    weather_code: String,
+}
+
 impl WeatherDisplay {
     fn new(weather_data: &WeatherResponse, display_name: &String) -> WeatherDisplay {
-        WeatherDisplay {
-            display_name: display_name.to_string(),
-            forecasts: weather_data
+        let utc_time_offset = weather_data.utc_offset_seconds;
+        let current_time = weather_data.current.time;
+
+        let now = DateTime::from_timestamp(current_time + utc_time_offset, 0)
+            .unwrap_or_else(|| panic!("Could not parse unixtime"));
+        let now_utc = DateTime::from_timestamp(current_time, 0)
+            .unwrap_or_else(|| panic!("Could not parse unixtime"));
+
+        let this_hour = Utc
+            .with_ymd_and_hms(
+                now_utc.year(),
+                now_utc.month(),
+                now_utc.day(),
+                now_utc.hour(),
+                0,
+                0,
+            )
+            .unwrap();
+        let this_hour = DateTime::from_timestamp(this_hour.timestamp() + utc_time_offset, 0)
+            .unwrap_or_else(|| panic!("Unwrap for this_hour failed"));
+
+        let today = Utc
+            .with_ymd_and_hms(now_utc.year(), now_utc.month(), now_utc.day() - 1, 0, 0, 0)
+            .unwrap();
+        let today = DateTime::from_timestamp(today.timestamp() + utc_time_offset, 0)
+            .unwrap_or_else(|| panic!("unwrap for today failed"));
+
+        let current_date_tuple = (now.year(), now.month(), now.day());
+        let daily_forecasts: Vec<DailyForecastWithDateTime> = weather_data
+            .daily
+            .time
+            .iter()
+            .zip(weather_data.daily.temperature_2m_max.iter())
+            .zip(weather_data.daily.temperature_2m_min.iter())
+            .zip(weather_data.daily.weather_code.iter())
+            .map(
+                |(((time, temp_max), temp_min), weather_code)| DailyForecastWithDateTime {
+                    date: DateTime::from_timestamp(time + utc_time_offset, 0)
+                        .unwrap_or_else(|| panic!("Could not parse unixtime")),
+                    temperature_min: *temp_min,
+                    temperature_max: *temp_max,
+                    weather_code: *weather_code,
+                },
+            )
+            .collect();
+        let todays_forcasts: Vec<&DailyForecastWithDateTime> = daily_forecasts
+            .iter()
+            .filter(|forecast| {
+                (
+                    forecast.date.year(),
+                    forecast.date.month(),
+                    forecast.date.day(),
+                ) == current_date_tuple
+            })
+            .take(1)
+            .collect();
+        let todays_forecast = todays_forcasts
+            .get(0)
+            .unwrap_or_else(|| panic!("Daily forecast not found"));
+
+        return WeatherDisplay {
+            display_name: display_name.split(",").take(1).collect(),
+            current: CurrentForecast {
+                time: now.format("%-l:%M %p").to_string(),
+                temp_max: todays_forecast.temperature_max,
+                temp_min: todays_forecast.temperature_min,
+            },
+            hourly: weather_data
                 .hourly
                 .time
                 .iter()
                 .zip(weather_data.hourly.temperature_2m.iter())
                 .zip(weather_data.hourly.weather_code.iter())
-                .map(|((date, temp_c), weather_code)| Forecast {
-                    date: date.to_string(),
-                    temperature: celsius_to_fahrenheit(temp_c),
+                .map(|((time, temp), weather_code)| HourlyForecastWithDateTime {
+                    date: DateTime::from_timestamp(time + utc_time_offset, 0)
+                        .unwrap_or_else(|| panic!("Could not parse unixtime")),
+                    temperature: *temp,
                     weather_code: weather_code.to_string(),
                 })
+                .filter(|hour| hour.date >= this_hour)
+                .map(|hour| HourlyForecast {
+                    date: hour.date.format("%-l %p").to_string(),
+                    temperature: hour.temperature,
+                    weather_code: hour.weather_code,
+                })
                 .collect(),
-        }
+            daily: weather_data
+                .daily
+                .time
+                .iter()
+                .zip(weather_data.daily.temperature_2m_min.iter())
+                .zip(weather_data.daily.temperature_2m_max.iter())
+                .zip(weather_data.daily.weather_code.iter())
+                .map(
+                    |(((time, temp_min), temp_max), weather_code)| DailyForecastWithDateTime {
+                        date: DateTime::from_timestamp(time + utc_time_offset, 0)
+                            .unwrap_or_else(|| panic!("Could not parse unixtime")),
+                        temperature_max: *temp_max,
+                        temperature_min: *temp_min,
+                        weather_code: *weather_code,
+                    },
+                )
+                .filter(|day| day.date >= today)
+                .map(|day| DailyForecast {
+                    date: day.date.format("%-m/%d").to_string(),
+                    temperature_min: day.temperature_min,
+                    temperature_max: day.temperature_max,
+                    weather_code: day.weather_code.to_string(),
+                })
+                .collect(),
+        };
     }
 }
 
@@ -95,7 +245,7 @@ impl WeatherDisplay {
 async fn get_weather(
     Query(params): Query<WeatherParams>,
     State(api_key): State<String>,
-) -> Result<WeatherDisplay, StatusCode> {
+) -> Result<Json<WeatherDisplay>, StatusCode> {
     let location_data = get_location_data(&params.zipcode, &api_key)
         .await
         .map_err(|_| StatusCode::NOT_FOUND)?;
@@ -106,7 +256,7 @@ async fn get_weather(
             return StatusCode::INTERNAL_SERVER_ERROR;
         })?;
     let weather_display = WeatherDisplay::new(&weather_data, &location_data.display_name);
-    Ok(weather_display)
+    Ok(Json(weather_display))
 }
 
 async fn fetch_weather(
@@ -114,7 +264,7 @@ async fn fetch_weather(
     lat: &str,
 ) -> Result<WeatherResponse, Box<dyn std::error::Error>> {
     let endpoint = format!(
-        "https://api.open-meteo.com/v1/forecast?latitude={}&longitude={}&hourly=temperature_2m,weather_code",
+        "https://api.open-meteo.com/v1/forecast?latitude={}&longitude={}&hourly=temperature_2m,weather_code&temperature_unit=fahrenheit&forecast_days=5&current=temperature_2m,weather_code&daily=temperature_2m_max,temperature_2m_min,weather_code&past_days=1&timezone=auto&timeformat=unixtime",
         lat, lon
     );
 
