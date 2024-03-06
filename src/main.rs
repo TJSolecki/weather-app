c62d00c4279e966b9b502b6b24bfc3f7b9f65334
Thomas Solecki <thomasjsolecki@gmail.com>
2024-03-06 15:19:14 -0800

    Reorganize main.rs and change hourly weather to go a full day ahead

--- src/main.rs
+++ src/main.rs
@@ -43,103 +43,174 @@ async fn main() {
         .unwrap();
 }
 
+fn get_weather_icon(
+    weather_map: &HashMap<String, String>,
+    weather_code: &str,
+    is_day: &u8,
+) -> String {
+    if *is_day == 0 && vec!["0", "1", "2", "3"].contains(&weather_code) {
+        return weather_map
+            .get(&format!("{}night", weather_code))
+            .unwrap_or_else(|| panic!("could not unwrap night icon"))
+            .clone();
+    }
+    weather_map
+        .get(weather_code)
+        .unwrap_or_else(|| panic!("could not unwrap icon"))
+        .clone()
+}
+#[axum_macros::debug_handler]
+async fn get_weather(
+    Query(params): Query<WeatherParams>,
+    State(local_state): State<LocalState>,
+) -> Result<WeatherDisplay, StatusCode> {
+    let location_data = get_location_data(&params.zipcode, &local_state.api_key)
+        .await
+        .map_err(|_| StatusCode::NOT_FOUND)?;
+    let weather_data = fetch_weather(&location_data.lon, &location_data.lat)
+        .await
+        .map_err(|err| {
+            println!("{:?}", err);
+            StatusCode::INTERNAL_SERVER_ERROR
+        })?;
+    let weather_display = WeatherDisplay::new(
+        &weather_data,
+        &location_data.display_name,
+        &local_state.weather_code_to_href,
+    );
+    Ok(weather_display)
+}
+
+async fn fetch_weather(
+    lon: &str,
+    lat: &str,
+) -> Result<WeatherResponse, Box<dyn std::error::Error>> {
+    let endpoint = format!(
+        "https://api.open-meteo.com/v1/forecast?latitude={}&longitude={}&hourly=temperature_2m,weather_code,is_day&temperature_unit=fahrenheit&forecast_days=5&current=temperature_2m,weather_code&daily=temperature_2m_max,temperature_2m_min,weather_code&past_days=1&timezone=auto&timeformat=unixtime",
+        lat, lon
+    );
+
+    let response = reqwest::get(&endpoint)
+        .await?
+        .json::<WeatherResponse>()
+        .await?;
+    Ok(response)
+}
+
+async fn get_location_data(
+    zipcode: &str,
+    api_key: &str,
+) -> Result<LocationData, Box<dyn std::error::Error>> {
+    let res = reqwest::get(format!(
+        "https://geocode.maps.co/search?q={}&api_key={}",
+        zipcode, api_key
+    ))
+    .await?
+    .json::<Vec<LocationData>>()
+    .await?;
+
+    res.get(0)
+        .cloned()
+        .ok_or("No results found for given zipcode".into())
+}
+
 #[derive(Clone)]
-struct LocalState {
-    api_key: String,
-    weather_code_to_href: HashMap<String, String>,
+pub struct LocalState {
+    pub api_key: String,
+    pub weather_code_to_href: HashMap<String, String>,
 }
 
 #[derive(Deserialize, Serialize, Debug, Clone)]
-struct LocationData {
-    display_name: String,
-    lon: String,
-    lat: String,
+pub struct LocationData {
+    pub display_name: String,
+    pub lon: String,
+    pub lat: String,
 }
 
 #[derive(Deserialize)]
-struct WeatherParams {
-    zipcode: String,
+pub struct WeatherParams {
+    pub zipcode: String,
 }
 
 #[derive(Serialize, Deserialize, Debug)]
-struct WeatherResponse {
-    latitude: f64,
-    longitude: f64,
-    utc_offset_seconds: i64,
-    timezone: String,
-    timezone_abbreviation: String,
-    current: Current,
-    hourly: Hourly,
-    daily: Daily,
+pub struct WeatherResponse {
+    pub latitude: f64,
+    pub longitude: f64,
+    pub utc_offset_seconds: i64,
+    pub timezone: String,
+    pub timezone_abbreviation: String,
+    pub current: Current,
+    pub hourly: Hourly,
+    pub daily: Daily,
 }
 
 #[derive(Serialize, Deserialize, Debug)]
-struct Current {
-    time: i64,
-    temperature_2m: f64,
-    weather_code: u16,
+pub struct Current {
+    pub time: i64,
+    pub temperature_2m: f64,
+    pub weather_code: u16,
 }
 
 #[derive(Serialize, Deserialize, Debug)]
-struct Hourly {
-    time: Vec<i64>,
-    temperature_2m: Vec<f64>,
-    weather_code: Vec<u16>,
-    is_day: Vec<u8>, // 1 for day 0 for night
+pub struct Hourly {
+    pub time: Vec<i64>,
+    pub temperature_2m: Vec<f64>,
+    pub weather_code: Vec<u16>,
+    pub is_day: Vec<u8>, // 1 for day 0 for night
 }
 
 #[derive(Serialize, Deserialize, Debug)]
-struct Daily {
-    time: Vec<i64>,
-    temperature_2m_max: Vec<f64>,
-    temperature_2m_min: Vec<f64>,
-    weather_code: Vec<u16>,
+pub struct Daily {
+    pub time: Vec<i64>,
+    pub temperature_2m_max: Vec<f64>,
+    pub temperature_2m_min: Vec<f64>,
+    pub weather_code: Vec<u16>,
 }
 
 #[derive(Template, Serialize, Deserialize, Debug)]
 #[template(path = "weather.html")]
-struct WeatherDisplay {
-    display_name: String,
-    current: CurrentForecast,
-    hourly: Vec<HourlyForecast>,
-    daily: Vec<DailyForecast>,
+pub struct WeatherDisplay {
+    pub display_name: String,
+    pub current: CurrentForecast,
+    pub hourly: Vec<HourlyForecast>,
+    pub daily: Vec<DailyForecast>,
 }
 
 #[derive(Serialize, Deserialize, Debug)]
-struct CurrentForecast {
-    time: String,
-    temp_max: i32,
-    temp_min: i32,
-    temp: i32,
+pub struct CurrentForecast {
+    pub time: String,
+    pub temp_max: i32,
+    pub temp_min: i32,
+    pub temp: i32,
 }
 
 #[derive(Serialize, Deserialize, Debug)]
-struct HourlyForecast {
-    date: String,
-    temperature: i32,
-    weather_code: String,
+pub struct HourlyForecast {
+    pub date: String,
+    pub temperature: i32,
+    pub weather_code: String,
 }
 
 #[derive(Serialize, Deserialize, Debug)]
-struct DailyForecast {
-    date: String,
-    temperature_min: i32,
-    temperature_max: i32,
-    weather_code: String,
+pub struct DailyForecast {
+    pub date: String,
+    pub temperature_min: i32,
+    pub temperature_max: i32,
+    pub weather_code: String,
 }
 
-struct DailyForecastWithDateTime {
-    date: DateTime<Utc>,
-    temperature_min: f64,
-    temperature_max: f64,
-    weather_code: u16,
+pub struct DailyForecastWithDateTime {
+    pub date: DateTime<Utc>,
+    pub temperature_min: f64,
+    pub temperature_max: f64,
+    pub weather_code: u16,
 }
 
-struct HourlyForecastWithDateTime {
-    date: DateTime<Utc>,
-    temperature: f64,
-    weather_code: String,
-    is_day: u8,
+pub struct HourlyForecastWithDateTime {
+    pub date: DateTime<Utc>,
+    pub temperature: f64,
+    pub weather_code: String,
+    pub is_day: u8,
 }
 
 impl WeatherDisplay {
@@ -262,7 +333,7 @@ impl WeatherDisplay {
                         &hour.is_day,
                     ),
                 })
-                .take(24)
+                .take(25)
                 .collect(),
             daily: weather_data
                 .daily
@@ -294,76 +365,4 @@ impl WeatherDisplay {
                 .collect(),
         }
     }
-}
-
-fn get_weather_icon(
-    weather_map: &HashMap<String, String>,
-    weather_code: &str,
-    is_day: &u8,
-) -> String {
-    if *is_day == 0 && vec!["0", "1", "2", "3"].contains(&weather_code) {
-        return weather_map
-            .get(&format!("{}night", weather_code))
-            .unwrap_or_else(|| panic!("could not unwrap night icon"))
-            .clone();
-    }
-    weather_map
-        .get(weather_code)
-        .unwrap_or_else(|| panic!("could not unwrap icon"))
-        .clone()
-}
-
-#[axum_macros::debug_handler]
-async fn get_weather(
-    Query(params): Query<WeatherParams>,
-    State(local_state): State<LocalState>,
-) -> Result<WeatherDisplay, StatusCode> {
-    let location_data = get_location_data(&params.zipcode, &local_state.api_key)
-        .await
-        .map_err(|_| StatusCode::NOT_FOUND)?;
-    let weather_data = fetch_weather(&location_data.lon, &location_data.lat)
-        .await
-        .map_err(|err| {
-            println!("{:?}", err);
-            return StatusCode::INTERNAL_SERVER_ERROR;
-        })?;
-    let weather_display = WeatherDisplay::new(
-        &weather_data,
-        &location_data.display_name,
-        &local_state.weather_code_to_href,
-    );
-    Ok(weather_display)
-}
-
-async fn fetch_weather(
-    lon: &str,
-    lat: &str,
-) -> Result<WeatherResponse, Box<dyn std::error::Error>> {
-    let endpoint = format!(
-        "https://api.open-meteo.com/v1/forecast?latitude={}&longitude={}&hourly=temperature_2m,weather_code,is_day&temperature_unit=fahrenheit&forecast_days=5&current=temperature_2m,weather_code&daily=temperature_2m_max,temperature_2m_min,weather_code&past_days=1&timezone=auto&timeformat=unixtime",
-        lat, lon
-    );
-
-    let response = reqwest::get(&endpoint)
-        .await?
-        .json::<WeatherResponse>()
-        .await?;
-    Ok(response)
-}
-
-async fn get_location_data(
-    zipcode: &str,
-    api_key: &str,
-) -> Result<LocationData, Box<dyn std::error::Error>> {
-    let res = reqwest::get(format!(
-        "https://geocode.maps.co/search?q={}&api_key={}",
-        zipcode, api_key
-    ))
-    .await?
-    .json::<Vec<LocationData>>()
-    .await?;
-
-    res.get(0)
-        .cloned()
-        .ok_or("No results found for given zipcode".into())
 }
