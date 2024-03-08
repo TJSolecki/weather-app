4927e6ba0f60f42152bd0107a2631befa24c0747
Thomas Solecki <thomasjsolecki@gmail.com>
2024-03-08 11:48:20 -0800

    Change host address to 0.0.0.0:3000

--- src/main.rs
+++ src/main.rs
@@ -34,10 +34,8 @@ async fn main() {
             weather_code_to_href,
         });
 
-    let listener = tokio::net::TcpListener::bind("127.0.0.1:3000")
-        .await
-        .unwrap();
-    println!("listening on http://{}", "127.0.0.1:3000");
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+    println!("listening on http://{}", "0.0.0.0:3000");
     axum::serve(listener, app.into_make_service())
         .await
         .unwrap();
